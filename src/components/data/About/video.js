de91cb9582460a8acbe0e6e7fde8a6dea022b2dd
Rifkynur <ythrifky619@gmail.com>
2024-08-20 19:31:49 +0700

    feat:add video section in about page

---
+++ src/components/data/About/video.js
@@ -0,0 +1,24 @@
+import Icon1 from "../../../assets/About/restaurant-menu 1.svg";
+import Icon2 from "../../../assets/About/Icon.svg";
+import Icon3 from "../../../assets/About/timer 1.svg";
+
+export const listVideo = [
+  {
+    id: 1,
+    title: "Multi Cuisine",
+    desc: "In the new era of technology we look in the future with certainty life.",
+    icon: Icon1,
+  },
+  {
+    id: 2,
+    title: "Easy To Order",
+    desc: "In the new era of technology we look in the future with certainty life.",
+    icon: Icon2,
+  },
+  {
+    id: 3,
+    title: "Fast Delivery",
+    desc: "In the new era of technology we look in the future with certainty life.",
+    icon: Icon3,
+  },
+];
--- src/components/page/About/Video.jsx
+++ src/components/page/About/Video.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 import bgVideo from "../../../assets/About/bgVideo.png";
+import { IoMdPlayCircle } from "react-icons/io";
+import Icon1 from "../../../assets/About/restaurant-menu 1.svg";
+import { listVideo } from "../../data/About/video";
 
 const Video = () => {
   return (
     <section>
-      <div className="h-28 w-full bg-cover bg-center" style={{ backgroundImage: `url(${bgVideo})` }}>
-        asd
+      <div className="flex h-52 w-full flex-col items-center justify-center gap-4 bg-cover bg-center text-white md:h-72 lg:h-[690px]" style={{ backgroundImage: `url(${bgVideo})` }}>
+        <IoMdPlayCircle className="text-4xl md:text-[108px]" />
+        <h2 className="max-w-64 text-center font-[playFair] text-xl font-medium md:max-w-96 md:text-4xl lg:max-w-xl lg:text-h2">Feel the authentic & original taste from us</h2>
       </div>
-      <div></div>
+      <div className="flex flex-col justify-center gap-4 bg-white p-2 md:flex-row md:px-4 md:py-12 lg:py-20">
+        {listVideo.map((item) => {
+          return (
+            <div key={item.id} className="flex items-start gap-1 md:gap-2 lg:gap-6">
+              <div>
+                <img src={item.icon} alt={item.title} className="size-8 lg:size-12" />
+              </div>
+              <div>
+                <h3 className="mb-2 text-sm font-bold lg:mb-4 lg:text-paragraph3">{item.title}</h3>
+                <p className="max-w-64 text-sm lg:text-paragraph2">{item.desc}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </section>
   );
 };
